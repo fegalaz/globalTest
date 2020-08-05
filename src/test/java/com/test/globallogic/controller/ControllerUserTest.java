5c49a757478c437279951b771b34d3a58de4650b
Mel <ext_msromero@falabella.cl>
2020-08-05 16:49:57 -0400

    Se agregan test unitarios.

--- src/test/java/com/test/globallogic/controller/ControllerUserTest.java
+++ src/test/java/com/test/globallogic/controller/ControllerUserTest.java
@@ -2,8 +2,12 @@ package com.test.globallogic.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import com.test.globallogic.dto.PhoneDTO;
+import com.test.globallogic.util.ValidateEmail;
+import com.test.globallogic.util.ValidatePassword;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -14,11 +18,19 @@ import org.springframework.http.ResponseEntity;
 import com.test.globallogic.dto.UserDTO;
 import com.test.globallogic.service.UserService;
 
+import java.util.Collections;
+
 public class ControllerUserTest {
 	
 	@Mock
     private UserService userService;
-	
+
+	@Mock
+    private ValidateEmail validateEmail;
+
+	@Mock
+    private ValidatePassword validatePass;
+
 	@InjectMocks
     private ControllerUser controllerUser;
 	    
@@ -29,13 +41,22 @@ public class ControllerUserTest {
     }
 
     @Test
-    void createUser() {
+    void createUserOK() {
 
-    	UserDTO user = new UserDTO();
-    	user.setName("Felipe Galaz");
-    	user.setEmail("fe.galaz@gmail.com");
-    	user.setPassword("Hunt12");
-    	
+        UserDTO user = new UserDTO();
+        PhoneDTO phone = new PhoneDTO();
+        user.setName("Felipe Galaz");
+        user.setEmail("asasasa@rodriguez.org");
+        user.setPassword("Hunt12");
+        phone.setNumber("1234567");
+        phone.setCityCode("1");
+        phone.setCountryCode("57");
+        user.setPhoneDTOs(Collections.singletonList(phone));
+
+        when(validateEmail.isValid(anyString())).thenReturn(Boolean.TRUE);
+
+        when(validatePass.isValid(anyString())).thenReturn(Boolean.TRUE);
+
         when(userService.createUser(any())).thenReturn(user);
 
         ResponseEntity<?> result = controllerUser.createUser(user);
